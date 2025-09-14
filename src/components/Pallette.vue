ff1bd393c0edd5398dc2c8b19d4c7ce4dca7a931
Jean Lionel <nijeanlionel@gmail.com>
2025-09-14 19:38:56 +0200

    add urubugu component

---
+++ src/components/Pallette.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    :style="{
+      display: 'grid',
+      gridTemplateColumns: 'repeat(8, 0fr)',
+      gap: 0,
+      padding: 0,
+    }"
+  >
+    <img
+      v-for="(caseValue, index) in cases"
+      :key="index"
+      :style="{
+        filter: `opacity(0.80) drop-shadow(0 0 0 ${user.color})`,
+        cursor: 'pointer',
+      }"
+      class="cart"
+      :src="`/src/assets/images/${caseValue}.png`"
+      alt="Cart"
+    />
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { ref } from 'vue'
+
+const props = defineProps({
+  user: {
+    type: Object,
+    required: true,
+  },
+})
+
+const cases = computed(() => {
+  return props.user.cases.map((e) =>
+    isNaN(e) || e === null || e === undefined || e === '' || e === '0' || e > 32
+      ? 0
+      : parseInt(e) > 15
+        ? 15
+        : parseInt(e),
+  )
+})
+</script>
+
+<style scoped>
+.cart {
+  /* Add any additional styling for the cart class here if needed */
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Pallette from '@/components/Pallette.vue'
+import { ref } from 'vue'
+
+const user = ref({
+  color: '#FF0000',
+  cases: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+  name: 'John Doe',
+})
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Pallette :user="user" />
   </main>
 </template>
